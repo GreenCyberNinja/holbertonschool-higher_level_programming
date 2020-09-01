d0e09b1d8b5ac54154debd41c7ec58e6dda29112
GreenCyberNinja <1824@holbertonschool.com>
2020-09-01 19:36:56 +0000

    fixed zero to 0

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -4,6 +4,6 @@ number = random.randint(-10, 10)
 if number < 0:
     print('{:d} is negative'.format(number))
 elif number == 0:
-    print('{:d} is zero'.format(number))
+    print('{:d} is 0'.format(number))
 else:
     print('{:d} is positve'.format(number))
