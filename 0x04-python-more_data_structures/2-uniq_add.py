835d36fa31739bc704a77216a6772bdf64278e48
GreenCyberNinja <1824@holbertonschool.com>
2020-09-10 21:04:42 +0000

    went with a set instead of a sum

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    new = my_list[:]
+    new = list(set(my_list))
     b = 0
     for i in new:
-        a = new[i]
-        if new.count(a) > 1:
-            new.remove(new[i])
-    return (sum(new))
+        b += i
+    return (b)
