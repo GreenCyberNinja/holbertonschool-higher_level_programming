52420675be92d0f20707422b9935f4e32c9abac0
GreenCyberNinja <1824@holbertonschool.com>
2020-09-23 01:13:51 +0000

    pep8 and doc compliant

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Program to create class square"""
+
+
+class Square:
+    """class establish Square"""
+    def __init__(self, size):
+        """initialize size as private atribute"""
+        self.__sizei = size
