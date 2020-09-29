f3f34c861433d0ab65c499e1810c515b5cc7174e
GreenCyberNinja <1824@holbertonschool.com>
2020-09-29 01:40:20 +0000

    got rid of faulty commits

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,12 +9,10 @@ class Rectangle:
         self.height = height
 
     @property
-    """height getter"""
     def height(self):
         return self.__height
 
     @height.setter
-    """height setter"""
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
@@ -24,12 +22,10 @@ class Rectangle:
             self.__height = value
 
     @property
-    """height getter"""
     def width(self):
         return self.__width
 
     @width.setter
-    """height setter"""
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
