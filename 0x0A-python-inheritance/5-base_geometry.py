14ca3ce281cf98fdda785cf158ed520b9415fbea
GreenCyberNinja <1824@holbertonschool.com>
2020-10-07 04:29:59 +0000

    answer push 2

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""file for task5"""
+
+
+class BaseGeometry():
+    """ empty class"""
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""file for task5"""
+
+
+class BaseGeometry():
+   
+    def __init__(self):
+        pass
+
+    def area(self):
+        raise(Exception("area() is not implemented"))
