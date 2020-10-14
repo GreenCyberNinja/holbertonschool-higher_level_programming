4ff91c52eadac472ec554eb9ad2a9a538c538801
GreenCyberNinja <1824@holbertonschool.com>
2020-10-14 05:06:24 +0000

    pep8 and doclines

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,8 +4,10 @@ from models.base import Base
 
 
 class Rectangle(Base):
+    """class Rectangle"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """initializes"""
         super().__init__(id)
         self.width = width
         self.height = height
@@ -13,9 +15,11 @@ class Rectangle(Base):
         self.y = y
 
     def area(self):
+        """establishes the area"""
         return self.width * self.height
 
     def display(self):
+        """displays the rectangle"""
         for h in range((self.y + self.height)):
             for w in range((self.x + self.width)):
                 if h < self.y:
@@ -27,7 +31,8 @@ class Rectangle(Base):
             print()
 
     def update(self, *args, **kwargs):
-        if len(args) > 0 :
+        """updates the attributes"""
+        if args:
             for arg in range(args):
                 if arg == 0:
                     self.id = args[arg]
@@ -45,27 +50,30 @@ class Rectangle(Base):
                     self.id = kwargs['id']
                 if karg == 'width':
                     self.width = kwargs['width']
-                if karg =='height':
+                if karg == 'height':
                     self.height = kwargs['height']
                 if karg == 'x':
                     self.x = kwargs['x']
                 if karg == 'y':
                     self.y = kwargs['y']
 
-
     def __str__(self):
+        """returns the string representation"""
         return("[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}".format
-
                (self.id, self.x, self.y, self.width, self.height))
+
     def to_dictionary(self):
+        """returns dictionary"""
         return self.__dict__
 
     @property
     def height(self):
+        """height getter"""
         return self.__height
 
     @height.setter
     def height(self, height):
+        """height setter"""
         if type(height) is not int:
             raise TypeError("height must be an integer")
         if height <= 0:
@@ -74,10 +82,12 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """width getter"""
         return self.__width
 
     @width.setter
     def width(self, width):
+        """width setter"""
         if type(width) is not int:
             raise TypeError("width must be an integer")
         if width <= 0:
@@ -86,10 +96,12 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """x getter"""
         return self.__x
 
     @x.setter
     def x(self, x):
+        """x setter"""
         if type(x) is not int:
             raise TypeError("x must be an integer")
         if x < 0:
@@ -99,10 +111,12 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        """y getter"""
         return self.__y
 
     @y.setter
     def y(self, y):
+        """y setter"""
         if type(y) is not int:
             raise TypeError("y must be an integer")
         if y < 0:
