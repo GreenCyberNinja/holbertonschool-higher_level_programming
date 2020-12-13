0f28b4d80b149dace89aff66c4c1fceff49190b1
GreenCyberNinja <1824@holbertonschool.com>
2020-12-13 02:34:22 +0000

    trying cause the test table doesn't work

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""lists all states with a name starting with N"""
+import MySQLdb
+import sys
+
+
+def getselect_states():
+    """requires usr password and table returns all states"""
+    daba = MySQLdb.connect(host="localhost",
+                           port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = daba.cursor()
+    cmd = "SELECT cities.id, cities.name, states.name FROM cities\
+           INNER JOIN states ON cities.state_id=states.id\
+           ORDER BY cities.id ASC;"
+    cur.execute(cmd)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        state = row[1]
+        print(row)
+
+    cur.close()
+    daba.close()
+
+
+if __name__ == "__main__":
+    getselect_states()
