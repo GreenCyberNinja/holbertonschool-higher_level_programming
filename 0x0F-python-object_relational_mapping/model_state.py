e6c37d8428c4447fa22d7c33c1300035c419ba44
GreenCyberNinja <1824@holbertonschool.com>
2020-12-14 05:12:49 +0000

    spelt declarative wrong

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,14 +2,14 @@
 """model for state table"""
 
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declaritive import declaritive_base
+from sqlachemy.ext.declarative import declarative_base
 
-Base = declaritive_base()
+Base = declarative_base()
 
 
 class State(Base):
     """class State name and id"""
     __tablename__ = "states"
 
-    id = Column(Integer, nullable=False, primary_key=True)
+    id = Column(Integer, nullable=False, autoincrement=True, primary_key=True)
     name = Column(String(128), nullable=False)
