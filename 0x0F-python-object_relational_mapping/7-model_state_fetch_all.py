6a2e54449ab554a390728c4802f39a2ef857beb6
GreenCyberNinja <1824@holbertonschool.com>
2020-12-14 04:21:56 +0000

    uses mysqlalchemy to get all states

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""displays all states with sqlalchemy"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def printall_states():
+    """prints all the states"""
+    ngin = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                         .format(argv[1], argv[2] argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=e)
+    s = Session()
+    for state in s.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
+    s.close()
+
+
+if __name__ == "__main__":
+    printall_states()
