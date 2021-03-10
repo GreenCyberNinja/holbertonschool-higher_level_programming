261295ca7e0b87eae788b1991d4ba4a255bc0c6a
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-03-10 07:59:02 +0000

    okay figured out what they actually wanted was a dynamic symbol str rep

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -6,6 +6,7 @@ class Rectangle:
     """class Rectangle atributes width and height"""
 
     number_of_instances = 0
+    rep_symbol = '#'
 
     def __init__(self, width=0, height=0):
         self.width = width
@@ -18,7 +19,7 @@ class Rectangle:
             return(string)
         for x in range(self.height):
             for y in range(self.width):
-                string += '#'
+                string += str(rep_symbol)
             if x is not self.height - 1:
                 string += '\n'
         return(string)
