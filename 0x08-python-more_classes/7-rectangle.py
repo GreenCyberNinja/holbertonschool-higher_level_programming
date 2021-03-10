8a888ad2551d290907fa06dec6bdcecc4ce18adf
GreenCyberNinja <green.cyber.ninja@gmail.com>
2021-03-10 07:52:12 +0000

    trying to change where i pull width and height

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -24,7 +24,7 @@ class Rectangle:
         return(string)
 
     def __repr__(self):
-        return("Rectangle({:d}, {:d})".format(self.width, self.height))
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
 
     def __del__(self):
         print("Bye rectangle...")
